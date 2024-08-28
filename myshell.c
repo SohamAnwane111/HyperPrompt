7c4a947b7cc4df141b86f75224a914bce5fb24cf
SohamAnwane111 <139840433+SohamAnwane111@users.noreply.github.com>
2024-08-28 19:37:18 +0530

    Added functionality to execute pipelined commands.

--- myshell.c
+++ myshell.c
@@ -5,40 +5,54 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+// constraints on number of commands & arguments.
 #define MAX_COMMANDS 10
 #define MAX_ARGS 10
 
+// flags to segreggate type of command.
 int exit_cond = 0;
 int num_commands = 0;
 int are_commands_sequential = 0;
 int are_commands_parallel = 0;
 int is_redirection = 0;
 int is_cd = 0;
+int is_pipeline = 0;
 
+// trap handler code for CTRL + C.
 void handle_sigint(int signal)
 {
+	// typecasting from (int) -> (void) supresses the signal.
 	(void)signal;
 }
 
+// trap handler code for CTRL + Z.
 void handle_sigtstp(int signal)
 {
+	// typecasting from (int) -> (void) supresses the signal.
 	(void)signal;
 }
 
 char ***parseInput(char *ip_command)
 {
+	// 2-D matrix (variable column and row length) of strings.
 	char ***result = malloc(MAX_COMMANDS * sizeof(char **));
 	int commandIndex = 0;
 
+	// flipping sequential command flag.
 	if (strstr(ip_command, "##") != NULL)
 		are_commands_sequential = 1;
 
+	// flipping parallel command flag.
 	if (strstr(ip_command, "&&") != NULL)
 		are_commands_parallel = 1;
 
+	// flipping pipeline command flag.
+	if (strstr(ip_command, "|") != NULL)
+		is_pipeline = 1;
+
 	char *command;
 	char *rest = ip_command;
-	while ((command = strsep(&rest, "&&##")) != NULL)
+	while ((command = strsep(&rest, "&&##|")) != NULL)
 	{
 		if (*command != '\0')
 		{
@@ -50,12 +64,18 @@ char ***parseInput(char *ip_command)
 			{
 				if (*arg != '\0')
 				{
+					// flipping exit command flag.
 					if (strcmp(arg, "exit") == 0)
 						exit_cond = 1;
+
+					// flipping redirection command flag.
 					if (strcmp(arg, ">") == 0)
 						is_redirection = 1;
+
+					// flipping change directory command flag/
 					if (strcmp(arg, "cd") == 0)
 						is_cd = 1;
+
 					result[commandIndex][argIndex++] = strdup(arg);
 				}
 			}
@@ -78,7 +98,7 @@ void executeCommand(char **arg_list)
 		int i = 0;
 		while (arg_list[i] != NULL)
 		{
-			size_t len = strlen(arg_list[i]);
+			int len = strlen(arg_list[i]);
 			if (len > 0 && arg_list[i][len - 1] == '\n')
 			{
 				arg_list[i][len - 1] = '\0';
@@ -107,6 +127,7 @@ void executeCommand(char **arg_list)
 	}
 	else
 	{
+		is_cd = 0;
 		if (chdir(arg_list[1]) != 0)
 		{
 			printf("Shell: No such directory exists\n");
@@ -123,11 +144,15 @@ void executeParallelCommands(char ***arg_list)
 		{
 			if (execvp(arg_list[i][0], arg_list[i]) == -1)
 			{
+				// in case if execvp() fails.
 				printf("Shell: Incorrect command\n");
 				exit(1);
 			}
 		}
 	}
+
+	// these wait(NULL)s prevents the sub_commands from being orphan process.
+	// ignoring these will kill the shell program before-hand.
 	for (int i = 0; i < num_commands; i++)
 	{
 		wait(NULL);
@@ -138,22 +163,35 @@ void executeSequentialCommands(char ***arg_list)
 {
 	for (int i = 0; i < num_commands; i++)
 	{
-		int rc = fork();
-		if (rc == 0)
+		if (strcmp(arg_list[i][0], "cd") == 0)
 		{
-			if (execvp(arg_list[i][0], arg_list[i]) == -1)
-			{
-				printf("Shell: Incorrect command\n");
-				exit(1);
-			}
-		}
-		else if (rc > 0)
-		{
-			wait(NULL);
+			is_cd = 1;
+			executeCommand(arg_list[i]);
 		}
 		else
 		{
-			exit(1);
+			int rc = fork();
+			if (rc == 0)
+			{
+				// Child process executes other commands.
+				if (execvp(arg_list[i][0], arg_list[i]) == -1)
+				{
+					// If execvp() fails.
+					printf("Shell: Incorrect command\n");
+					exit(1);
+				}
+			}
+			else if (rc > 0)
+			{
+				// Parent process waits for the child to finish.
+				wait(NULL);
+			}
+			else
+			{
+				// If fork() fails.
+				printf("Shell: Fork failed\n");
+				exit(1);
+			}
 		}
 	}
 }
@@ -181,38 +219,124 @@ void executeCommandRedirection(char **arg_list)
 
 			arg_list[i] = NULL;
 
-			execvp(arg_list[0], arg_list) == -1;
+			execvp(arg_list[0], arg_list);
 			exit(1);
 		}
 		else
 		{
+			// if input redirection command is incorrect.
 			exit(1);
 		}
 	}
 	else
 	{
+		// in case if fork() fails.
 		wait(NULL);
 	}
 }
 
+void executePipelineCommands(char ***arg_list)
+{
+    int num_pipes = num_commands - 1;
+    int pipefds[2 * num_pipes];
+
+    // Creating pipes.
+    for (int i = 0; i < num_pipes; i++)
+    {
+        if (pipe(pipefds + i * 2) < 0)
+        {
+            perror("Shell: Pipe creation failed");
+            exit(1);
+        }
+    }
+
+    int commandIndex = 0;
+    while (arg_list[commandIndex] != NULL)
+    {
+        int pid = fork();
+
+        if (pid == 0)
+        {
+            // If not the first command, getting input from the previous pipe.
+            if (commandIndex > 0)
+            {
+                if (dup2(pipefds[(commandIndex - 1) * 2], STDIN_FILENO) < 0)
+                {
+                    perror("Shell: dup2 failed");
+                    exit(1);
+                }
+            }
+
+            // If not the last command, output to the next pipe.
+            if (arg_list[commandIndex + 1] != NULL)
+            {
+                if (dup2(pipefds[commandIndex * 2 + 1], STDOUT_FILENO) < 0)
+                {
+                    perror("Shell: dup2 failed");
+                    exit(1);
+                }
+            }
+
+            // Close all pipes in the child process.
+            for (int i = 0; i < 2 * num_pipes; i++)
+            {
+                close(pipefds[i]);
+            }
+
+            // Execute command.
+            if (execvp(arg_list[commandIndex][0], arg_list[commandIndex]) == -1)
+            {
+                perror("Shell: Command execution failed");
+                exit(1);
+            }
+        }
+        else if (pid < 0)
+        {
+            perror("Shell: Fork failed");
+            exit(1);
+        }
+
+        commandIndex++;
+    }
+
+    // Parent process must close all pipes.
+    for (int i = 0; i < 2 * num_pipes; i++)
+    {
+        close(pipefds[i]);
+    }
+
+    // Parent must wait for all child processes.
+    for (int i = 0; i < num_commands; i++)
+    {
+        wait(NULL);
+    }
+}
 int main()
 {
 	while (1)
 	{
+		// triggers the trap handler code defined above.
 		signal(SIGINT, handle_sigint);
 		signal(SIGTSTP, handle_sigtstp);
 
-		char cwd[1024];
+		char current_working_directory[1024];
 
-		if (getcwd(cwd, sizeof(cwd)) != NULL)
-			printf("%s$ ", cwd);
+		// using cwd(), fetching the current working directory.
+		if (getcwd(current_working_directory, sizeof(current_working_directory)) != NULL)
+			printf("%s$", current_working_directory);
 
+		// using getline(), fetching the raw user given command.
 		char *ip_command = NULL;
 		size_t len = 0;
 		getline(&ip_command, &len, stdin);
 
 		ip_command[strcspn(ip_command, "\n")] = '\0';
 
+		// how does parseInput() processes the raw user command -->
+		// 1) Breaks the command with delimiter "##", "&&", or "|"
+		// 2) Now we get sub-commands from step-1, It breaks this sub-commands with delimiter " ".
+		// 3) thus, we obtain a 2-D matrix of strings.
+		// 4) where each rows represents a complete sub-command.
 		char ***arg_list = parseInput(ip_command);
 
 		if (exit_cond)
@@ -237,7 +361,12 @@ int main()
 		{
 			executeCommand(arg_list[0]);
 		}
+		else if(is_pipeline)
+		{
+			executePipelineCommands(arg_list);
+		}
 
+		// freeing the memory/resources as a single user command terminates.
 		for (int i = 0; arg_list[i] != NULL; i++)
 		{
 			for (int j = 0; arg_list[i][j] != NULL; j++)
@@ -249,6 +378,7 @@ int main()
 		free(arg_list);
 		free(ip_command);
 
+		// flipping all the flags to 0 after single user command termination.
 		num_commands = 0;
 		are_commands_parallel = 0;
 		are_commands_sequential = 0;
